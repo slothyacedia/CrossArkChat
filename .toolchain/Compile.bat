9fc2f934c89233d6040618a932d52b21e2ebca75
Acedia <110048632+slothyacedia@users.noreply.github.com>
2026-05-12 19:48:13 +0800

    remove the option to compile to a exe, since plugins still require nodejs modules

--- .toolchain/Compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-cd /d "%~dp0.."
-set "ROOT=%cd%"
-echo Project Root: %ROOT%
-echo.
-call pkg "%ROOT%\CrossArkChat.js" --targets node18-win-x64 --output "%~dp0CrossArkChat.exe"
-echo Done! Exit code: %errorlevel%
-pause
